bbd33ba4f567dbca7647bc8315b38f4320eb3b15
riham8899 <riham.ragab42@gmail.com>
2025-08-26 02:20:37 +0300

    modify routing erorr

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+//https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/A-HOME-assignment-RE/",
+
+   build: {
+    rollupOptions: {
+      input: 'index.html'
+    }
+  }
 })
+
+
+
+
